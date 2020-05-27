a9e17e287dc21ba546be1522f54ccc8d1b8aa690
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-05-27 16:04:37 +0900

    Add files via upload

---
+++ [[specialSave]]/20200527_1557/product/productSpec.php
@@ -0,0 +1,402 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/drClean/common/pages/head.php';
+?>
+
+<body>
+<div class="container">
+	<?php include $_SERVER['DOCUMENT_ROOT'] . '/drClean/common/pages/headerG.php'; ?>
+	<div class="contents">
+		<div class="section productBuySect">
+			<div class="sectionCon">
+				<div class="leftPartGroup productImgSort">
+					<img src="/drClean/common/img/img_goods3.png"/>
+				</div>
+				<div class="rightPartGroup buyControlSort">
+					<div class="titleBox productTitleSort">
+						<div class="iconGroup">
+							<a href="javascript:void(0);" class="iconBtn favoriteIcon"></a>
+						</div>
+						<div class="goodsName">[플루건] 인체무해 공간 순수 용제*3입</div>
+						<div class="priceBox">49,000원</div>
+					</div>
+					<div class="noticeInfoBox">
+						<span class="titleBox">배송비</span>
+						<span class="infoTextBox">2,500원(50,000원 이상 무료배송)</span>
+					</div>
+					<div class="selectOpGroup goodsOpSort">
+						<div class="optionGroup">
+							<div class="titleBox">옵션 선택 1</div>
+							<div class="sbox">
+								<a href="javascript:void(0);">선택해주세요.</a>
+								<ul>
+									<li>
+										<a href="javascript:void(0);">- 선택 -</a>
+									</li>
+									<li>
+										<a href="javascript:void(0);">옵션1</a>
+									</li>
+									<li>
+										<a href="javascript:void(0);">옵션2</a>
+									</li>
+									<li>
+										<a href="javascript:void(0);">옵션3</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="optionGroup">
+							<div class="titleBox">옵션 선택 2</div>
+							<div class="sbox">
+								<a href="javascript:void(0);">선택해주세요.</a>
+								<ul>
+									<li>
+										<a href="javascript:void(0);">- 선택 -</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<ul class="chosenOpGroup goodsOpSort">
+						<li class="optionBox">
+							<a href="javascript:void(0);" class="iconBtn closeIcon"></a>
+							<div class="titleBox">선택: [플루건] 인체무해 공간 순수용제*3입+200ml(리필)+세제500mlX2</div>
+							<div class="infoBox">
+								<div class="countBtnGroup">
+									<a href="javascript:void(0);" class="iconBtn">
+										<i class="minusIcon"></i>
+									</a>
+									<span>
+										<input class="tbox smallest onlyNum" value="1" onchange="" name="" maxlength="3">
+									</span>
+									<a href="javascript:void(0);" class="iconBtn">
+										<i class="plusIcon"></i>
+									</a>
+								</div>
+								<div class="priceBox optionSort">49,000원</div>
+							</div>
+						</li>
+						<li class="optionBox">
+							<a href="javascript:void(0);" class="iconBtn closeIcon"></a>
+							<div class="titleBox">선택: [플루건] 인체무해 공간 순수용제*2입+500ml(리필)+세제500mlX2</div>
+							<div class="infoBox">
+								<div class="countBtnGroup">
+									<a href="javascript:void(0);" class="iconBtn">
+										<i class="minusIcon"></i>
+									</a>
+									<span>
+										<input class="tbox smallest onlyNum" value="3" onchange="" name="" maxlength="3">
+									</span>
+									<a href="javascript:void(0);" class="iconBtn">
+										<i class="plusIcon"></i>
+									</a>
+								</div>
+								<div class="priceBox optionSort">51,000원</div>
+							</div>
+						</li>
+					</ul>
+					<div class="totalPriceGroup">
+						<div class="titleBox">총 결제 예상 금액</div>
+						<div class="priceBox">49,000원</div>
+					</div>
+
+					<div class="btnGroup buyControlSort">
+						<a href="javascript:void(0);" class="textBtn cartMod">장바구니</a>
+						<a href="javascript:void(0);" class="textBtn buyMod">바로구매</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="section productMainInfoSect">
+			<div class="sectionCon">
+				<div class="tabGroup primaryTabSort">
+					<a href="javascript:void(0);" class="goodsSpecSort activated">상품상세</a>
+					<a href="javascript:void(0);" class="goodsReviewSort">상품리뷰</a>
+					<a href="javascript:void(0);" class="goodsFaqSort">상품문의</a>
+					<a href="javascript:void(0);" class="deliveryRuleSort">배송정보</a>
+				</div>
+				<div class="verticalPartGroup goodsSpecSort">
+					<img src="/drClean/common/img/img_mainSlide1.png" class="mainInfoImg"/>
+				</div>
+				<div class="verticalPartGroup goodsReviewSort">
+					<div class="listHeadLine totalRateSort">
+						<div class="rateGroup">
+							<i class="starOnIcon greenMod"></i>
+							<i class="starOnIcon greenMod"></i>
+							<i class="starOnIcon greenMod"></i>
+							<i class="starOnIcon greenMod"></i>
+							<i class="starOffIcon greenMod"></i>
+						</div>
+						<div class="rateText">
+							<span class="currentRate">4.0</span>
+							<span class="rateLimit">/ 5</span>
+						</div>
+					</div>
+					<div class="listConGroup">
+						<ul class="reviewList">
+							<li>
+								<a href="javascript:void(0);" class="listTitleLine">
+									<div class="orderNumber">9</div>
+									<div class="rateGroup">
+										<i class="starOnIcon greenMod"></i>
+										<i class="starOnIcon greenMod"></i>
+										<i class="starOnIcon greenMod"></i>
+										<i class="starOnIcon greenMod"></i>
+										<i class="starOffIcon greenMod"></i>
+									</div>
+									<div class="infoBox">
+										<div class="reviewTitle">배송이 빨랐으면 좋겠어요. 배송이 빨랐으면 좋겠어요. 배송이 빨랐으면 좋겠어요.</div>
+										<div>[구매옵션] 세제 500ml</div>
+									</div>
+									<div class="userIdText">min******</div>
+									<div class="regDateText">2020-01-01</div>
+								</a>
+								<div class="listHiddenBox">
+									<div class="infoBox">
+										배송이 빨랐으면 좋겠어요 왜 그런지 모르겠지만 배송이 너무 느리고 답답하네요..<br/>
+										상품 만족도는 좋은데 아쉽습니다~
+									</div>
+								</div>
+							</li>
+							<li>
+								<a href="javascript:void(0);" class="listTitleLine">
+									<div class="orderNumber">9</div>
+									<div class="rateGroup">
+										<i class="starOnIcon greenMod"></i>
+										<i class="starOnIcon greenMod"></i>
+										<i class="starOnIcon greenMod"></i>
+										<i class="starOnIcon greenMod"></i>
+										<i class="starOffIcon greenMod"></i>
+									</div>
+									<div class="infoBox">
+										<div class="reviewTitle">배송이 빨랐으면 좋겠어요. 배송이 빨랐으면 좋겠어요. 배송이 빨랐으면 좋겠어요.</div>
+										<div>[구매옵션] 세제 500ml</div>
+									</div>
+									<div class="userIdText">min******</div>
+									<div class="regDateText">2020-01-01</div>
+								</a>
+								<div class="listHiddenBox">
+									<div class="infoBox">
+										배송이 빨랐으면 좋겠어요 왜 그런지 모르겠지만 배송이 너무 느리고 답답하네요..<br/>
+										상품 만족도는 좋은데 아쉽습니다~
+									</div>
+								</div>
+							</li>
+						</ul>
+						<div class="pageGroup">
+							<ul class="pageBox">
+								<!-- <li class="arrow prev"><a href="javascript:void(0);"></a></li> -->
+								<li class="pageNum activated"><a href="javascript:void(0);">1</a></li>
+								<li class="pageNum"><a href="javascript:void(0);">2</a></li>
+								<li class="pageNum"><a href="javascript:void(0);">3</a></li>
+								<!-- <li class="arrow next"><a href="javascript:void(0);"></a></li> -->
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="verticalPartGroup goodsFaqSort">
+					<div class="listHeadLine faqListSort">
+						<a href="javascript:void(0);" class="textBtn askSort">문의하기</a>
+					</div>
+					<div class="listConGroup">
+						<ul class="faqList">
+							<li>
+								<a href="javascript:void(0);" class="listTitleLine">
+									<div class="orderNumber">9</div>
+									<div class="faqStatus">답변완료</div>
+									<div class="infoBox faqListSort">
+										<div class="reviewTitle">재구매 했습니다.</div>
+									</div>
+									<div class="userIdText">min******</div>
+									<div class="regDateText">2020-01-01</div>
+								</a>
+								<div class="listHiddenBox faqListSort">
+									<div class="infoBox">
+										<div class="questionPart">
+											<div class="partMark">Q</div>
+											<div class="infoText">
+												배송이 빨랐으면 좋겠어요 왜 그런지 모르겠지만 배송이 너무 느리고 답답하네요..</br>
+												상품 만족도는 좋은데 아쉽습니다~
+											</div>
+										</div>
+										<div class="answerPart">
+											<div class="partMark">A</div>
+											<div class="infoText">
+												안녕하세요 고객님. </br>
+												우선 저희 닥터클린을 찾아주셔서 감사합니다.</br>
+												앞으로는 조금 더 빠르게 상품 받아보실 수 있도록 </br>
+												배송에 신경쓰겠습니다.</br>
+												감사합니다.</br>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+						</ul>
+						<div class="pageGroup">
+							<ul class="pageBox">
+								<!-- <li class="arrow prev"><a href="javascript:void(0);"></a></li> -->
+								<li class="pageNum activated"><a href="javascript:void(0);">1</a></li>
+								<li class="pageNum"><a href="javascript:void(0);">2</a></li>
+								<li class="pageNum"><a href="javascript:void(0);">3</a></li>
+								<!-- <li class="arrow next"><a href="javascript:void(0);"></a></li> -->
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="verticalPartGroup deliveryRuleSort">
+					<div class="emptySpace">배송 정보를 준비중입니다.</div>
+				</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'] . '/drClean/common/pages/footer.php'; ?>
+</div>
+<script>
+// :: ksg_20200522_1440 single : favorite 하트 버튼 번쩍번쩍.
+$(document).on('click', '.favoriteIcon', function(){
+	$(this).toggleClass('activated');
+});
+
+
+// :: ksg_20200526_1241 single 탭 선택 색상 변경 
+$(document).on('click', '.tabGroup.primaryTabSort a', function(){
+	$('.tabGroup.primaryTabSort a').removeClass('activated');
+	$(this).addClass('activated');
+});
+
+// :: ksg_20200526_1850 single : 리스트 클릭 시, 상세 내용 표시.
+$(document).on('click', 'a.listTitleLine', function(){
+	$(this).siblings('.listHiddenBox').slideToggle();
+});
+
+
+// :: ksg_20200526_1901 single : 페이징 버튼 클릭 스크립트.
+$(document).on('click', '.pageBox li a', function(){
+	$(this).parent('li').siblings().removeClass('activated');
+	$(this).parent('li').addClass('activated');
+});
+
+
+
+
+// :: ksg_20200527_1220 single : 탭 클릭 시, 적합한 섹션으로 이동하는 스크립트.
+$(document).ready(function() {
+  $(".tabGroup.primaryTabSort a").click(function() {
+	if ($(this).hasClass('goodsSpecSort')) {
+		$("body,html").animate(
+    	  { 
+    	    scrollTop: $(".verticalPartGroup.goodsSpecSort").offset().top - 60
+    	  },
+    	  800
+    	);
+	} else if ($(this).hasClass('goodsReviewSort')) {
+		$("body,html").animate(
+    	  { 
+    	    scrollTop: $(".verticalPartGroup.goodsReviewSort").offset().top - 60
+    	  },
+    	  800
+    	);
+	} else if ($(this).hasClass('goodsFaqSort')) {
+		$("body,html").animate(
+    	  { 
+    	    scrollTop: $(".verticalPartGroup.goodsFaqSort").offset().top - 60
+    	  },
+    	  800
+    	);
+	} else if ($(this).hasClass('deliveryRuleSort')) {
+		$("body,html").animate(
+    	  { 
+    	    scrollTop: $(".verticalPartGroup.deliveryRuleSort").offset().top - 60
+    	  },
+    	  800
+    	);
+	};
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// :: ksg_20200527_1221 open : 화면을 따라 탭이 움직이는 스크립트(일정 높이 이상에서는 고정으로 머물도록 함.)
+$(document).ready(function() {
+
+	$(window).scroll(function() {
+
+		// :: 탭이 올라갈 수 있는 한계 높이. (이 이상은 못 올라가고 멈춤.)
+		var limitTop = $(".section.productMainInfoSect").offset().top;
+
+		// :: 전체 페이지에서 현재 표시되는 화면이 차지하는 위치(페이지 꼭대기로부터 얼마나 먼 거리인가를 측정).
+		var scrollTop = $(window).scrollTop();
+
+		// :: 화면 스크롤 시, 탭이 위치해야 할 높이값을 계산. (현재 화면의 높이값 - 한계 고도(탭의 높이값에 한계 고도 위쪽의 불필요 공간 값이 섞여, 탭이 밑으로 가라앉는 문제를 해결하기 위해 한계 고도 값을 마이너스 처리 하는 것임.))
+		var newPosition = scrollTop - limitTop + "px";
+
+		// :: 애니메이션 들어간 졸졸이 따라다니는 스크립트. 
+		if(scrollTop > limitTop) {
+			$(".tabGroup.primaryTabSort").stop().animate({
+				"top" : newPosition		// :: 무빙 필요 구역에서 화면을 보고 있을 때 화면의 상단에 표시되도록 함.
+			}, 500);
+		} if(scrollTop < limitTop) {
+			$(".tabGroup.primaryTabSort").stop().animate({
+				"top" : 0		// :: 무빙 외 구역(상세내용 외적 부분) 화면을 보고 있을때는 무조건 한계 높이에 존재하도록 함.
+			});
+		}
+		
+	}).scroll();
+	
+});
+// :: ksg_20200527_1221 close : 화면을 따라 탭이 움직이는 스크립트(일정 높이 이상에서는 고정으로 머물도록 함.)
+
+
+
+
+</script>
+</body>
+</html>
